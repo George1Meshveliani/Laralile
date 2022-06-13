7e20d2fa75672f179de30664254aea3ca8f74c37
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-13 13:15:20 +0400

    change website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,13 +15,13 @@
                     <div class="pr-3"><strong> 1m </strong> followers </div>
                     <div class="pr-3"><strong>0 </strong> following</div>
                 </div>
+                <div class="pt-2 pl-3">
+                    I am a full stack web developer from Georgia. I have years of working experience in tech industry with my scientific background. Since COVID-19 changed our lives and we did appreciate internet more I decided to direct all my intellectual resources to the web development. Here you can see results.
+                </div>
                 <div style="color: blue" class="pt-4 pl-3">
                     <strong>
-                        meshveliani.netlify.app
+                        <a href="https://meshveliani.netlify.app/" target="_blank">www.meshveliani.dev</a>
                     </strong>
-                </div>
-                <div class="pt-2 pl-3">
-                    here is bio
                 </div>
             </div>
         </div>
