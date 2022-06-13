c586cdaef95e3d5f75f7703b8e155501e3eefae9
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-13 20:01:24 +0400

    addd field location

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -56,6 +56,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="location" class="col-md-4 col-form-label text-md-end">Location</label>
+
+                            <div class="col-md-6">
+                                <input id="location" type="location" class="form-control @error('location') is-invalid @enderror" name="location" value="{{ old('location') }}" required autocomplete="location">
+
+                                @error('location')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
