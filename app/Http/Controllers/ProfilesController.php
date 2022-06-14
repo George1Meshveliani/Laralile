79c88c633394b2df079d6b92a3f325a216e52d87
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-14 15:50:22 +0400

    change home controller to profile controller for user

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    //
+    public function index() {
+        return view('home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Auth::routes();
+//
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\ProfilesController::class, 'index'])->name('home');
