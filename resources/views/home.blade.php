bb6cb5961645b4f0a39c7106a029344490f32e4b
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-14 19:50:15 +0400

    Update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,37 +9,8 @@
                 <img src="https://www.etondigital.com/wp-content/uploads/2019/05/laravel-blog.png" class="rounded-3">
             </div>
             <div class="col-9 p-2 pt-5">
-                <div class="pl-3"><h1><strong>{{ $user->username }}</strong> </h1></div>
-                <div class="d-flex pl-3 pt-3">
-                    <div class="pr-3"><strong>508 </strong> announcements </div>
-                    <div class="pr-3"><strong> 1m </strong> followers </div>
-                    <div class="pr-3"><strong>0 </strong> following</div>
-                </div>
-                <div class="pt-2 pl-3">
-                    I am a full stack web developer from Georgia. I have years of working experience in tech industry with my scientific background. Since COVID-19 changed our lives and we did appreciate internet more I decided to direct all my intellectual resources to the web development. Here you can see results.
-                </div>
-                <div style="color: blue" class="pt-4 pl-3">
-                    <strong>
-                        <a href="https://meshveliani.netlify.app/" target="_blank">www.meshveliani.dev</a>
-                    </strong>
-                </div>
-            </div>
-        </div>
-        <div class="row pt-5">
-            <div class="col-4">
-                <img class="w-100 pt-3" src="https://i1.rgstatic.net/ii/profile.image/876708773240832-1586035068034_Q512/George-Meshveliani.jpg">
-            </div>
-            <div class="col-4">
-                <img class="w-100 pt-3" src="https://i1.rgstatic.net/ii/profile.image/876708773240832-1586035068034_Q512/George-Meshveliani.jpg">
-            </div>
-            <div class="col-4">
-                <img class="w-100 pt-3" src="https://i1.rgstatic.net/ii/profile.image/876708773240832-1586035068034_Q512/George-Meshveliani.jpg">
-            </div>
-            <div class="col-4">
-                <img class="w-100 pt-3" src="https://i1.rgstatic.net/ii/profile.image/876708773240832-1586035068034_Q512/George-Meshveliani.jpg">
+                <div class="pl-3"><h1><strong>{{ 'Hello ' . $user->name }}</strong> </h1></div>
             </div>
         </div>
     </div>
-
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Auth::routes();
-//
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
